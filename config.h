53b3395df4db62a67f544d929704ab491ff2e460
Noname Kamisawa <noname@kamisawa.net>
2022-03-31 22:18:41 +0900

    enable SHT31

--- config.h
+++ config.h
@@ -3,9 +3,9 @@
 // #define BOARD_M5CORE
 #define BOARD_M5CORE2
 
-// #define ENABLE_SHT31
+#define ENABLE_SHT31
 
 #ifdef ENABLE_SHT31
-#define SHT31_ADDRESS 0x45
-// #define SHT31_ADDRESS 0x44
+// #define SHT31_ADDRESS 0x45
+#define SHT31_ADDRESS 0x44
 #endif
